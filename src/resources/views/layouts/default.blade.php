fe445a1b0f147cede31372cd50a50adc2ea5ec90
Ryan Shofner <ryan@letsgolearn.com>
2015-07-03 18:57:36 -0700

    add default views

---
+++ src/resources/views/layouts/default.blade.php
@@ -0,0 +1,8 @@
+<div class="panel panel-default">
+    <div class="panel-heading"><h5>
+        @yield('title')
+        </h5></div>
+    <div class="panel-body">
+        @yield('body')
+    </div>
+</div>
---
+++ src/resources/views/test_widget.blade.php
@@ -0,0 +1,7 @@
+@section('title')
+    {{ $title !== '' ? $title : 'Test Widget' }}
+@endsection
+
+@section('body')
+    {{ $body !== '' ? $body : 'Test Widget' }}
+@endsection
